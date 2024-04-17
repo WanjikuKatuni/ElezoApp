8a0ba30bbfd7ba90c5339032b390d5b0b59b28b2
wanjiku <annritawanjiku77@gmail.com>
2024-04-17 19:56:01 +0300

    flask sqlalchemy setup

--- website/__init__.py
+++ website/__init__.py
@@ -2,10 +2,18 @@
 
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'ascbskfjefk fesmfawcflm' #encrypt cookie information for our data
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'  #show where the db is located
+    db.init_app(app) #initialise database 
 
     # import the blueprint variables
     from .views import views
