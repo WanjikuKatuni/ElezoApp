2d57bb4b0269473a2ad66048b5784ae2b9065691
wanjiku <annritawanjiku77@gmail.com>
2024-04-19 13:52:30 +0300

    set up one to many rship

--- website/models.py
+++ website/models.py
@@ -2,12 +2,13 @@ from . import db # import the defined db from the app
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-# create models
+# create models/tables
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 # user 
 class User(db.Model, UserMixin):
@@ -15,3 +16,4 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
