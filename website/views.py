ddb95b19e536c37de7f43588c620efa4d8552fd4
wanjiku <annritawanjiku77@gmail.com>
2024-04-25 07:52:42 +0300

    add notes

--- website/views.py
+++ website/views.py
@@ -1,14 +1,27 @@
 # standard routes(views) of the application aside from auth
 # blueprint organizes related views
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
+from .models import Note
+from . import db
 
 
 views = Blueprint('views', __name__ )
 
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required  #cannot get to home page if you have not logged in
 def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(text=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
+
     return render_template('home.html', user=current_user)
